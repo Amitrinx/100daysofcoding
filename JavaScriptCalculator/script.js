9515959116d54f1bb7f6721d405e01555999c7e4
Amit Tiwari <33163751+Amitrinx@users.noreply.github.com>
2018-08-07 00:29:05 +0530

    Update script.js

--- JavaScriptCalculator/script.js
+++ JavaScriptCalculator/script.js
@@ -1,4 +1,44 @@
-function GetValue(v)
+var vDisplay=null;
+var operator=null;
+function Result(v)
 {
-    document.getElementById("D").value=v;
+    var vDisplay = document.getElementById("D").value;
+    if(operator!=null)
+    {
+        if(operator=="+"){vDisplay=parseInt(vDisplay)+parseInt(v);}
+        if(operator=="-"){vDisplay=parseInt(vDisplay)-parseInt(v);}
+        if(operator=="/"){vDisplay=parseInt(vDisplay)/parseInt(v);}
+        if(operator=="*"){vDisplay=parseInt(vDisplay)*parseInt(v);}
+        operator=null;
+    }
+    else
+    {    
+        vDisplay+= v;
+    }
+    document.getElementById("D").value=vDisplay;
+}
+function AllClear(v)
+{
+    operator=null;
+    document.getElementById("D").value = '';
+    vDisplay=document.getElementById("D").value;
+}
+function Clear()
+{
+    var vDisplay = document.getElementById("D").value;
+    if(vDisplay!='' && vDisplay!=undefined && vDisplay!=null)
+    {
+        vDisplay= vDisplay.substring(0, vDisplay.length-1);
+    }
+    document.getElementById("D").value=vDisplay;
+}
+function Operator(v)
+{
+    operator=v;
+    document.getElementById("D").value += v;
+    
+}
+function GetResult()
+{
+    
 }
